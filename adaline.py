e43ad2cba4f1603677fefcf7ad470cb036c418da
Ved Nerlikar <64261977+ved164@users.noreply.github.com>
2022-07-22 01:46:30 +0200

    Update Adaline

--- adaline.py
+++ adaline.py
@@ -28,13 +28,13 @@ def __init__(self, eta = 0.01, n_iter = 50, random_state = 1):
     self.n_iter = n_iter
     self.random_state = random_state
 
-def fit(self, x, y):
+def fit(self, X, y):
     """Fit training data.
 
     Parameters:
     -------------
 
-    x : {array-like}, shape  [n_examples, n_features]
+    X : {array-like}, shape  [n_examples, n_features]
         Training vectors, where n_examples 
         is the number of examples and n_features is the number of features.
 
@@ -47,3 +47,44 @@ def fit(self, x, y):
 
     self : Object
     """
+    rgen = np.random.RandomState(self.random_state)
+    self.w_ = rgen.normal(loc=0.0, scale=0.01, size=X.shape[1])
+    self.b_ = np.float_(0.)
+    self.losses_ = []
+
+    for i in range(self.n_iter):
+        net_input = self.net_input(X)
+            # Please note that the "activation" method has no effect
+            # in the code since it is simply an identity function. We
+            # could write `output = self.net_input(X)` directly instead.
+            # The purpose of the activation is more conceptual, i.e.,  
+            # in the case of logistic regression (as we will see later), 
+            # we could change it to
+            # a sigmoid function to implement a logistic regression classifier.
+        output = self.activation(net_input)
+        errors = (y - output)
+            
+            #for w_j in range(self.w_.shape[0]):
+            #    self.w_[w_j] += self.eta * (2.0 * (X[:, w_j]*errors)).mean()
+            
+        self.w_ += self.eta * 2.0 * X.T.dot(errors) / X.shape[0]
+        self.b_ += self.eta * 2.0 * errors.mean()
+        loss = (errors**2).mean()
+        self.losses_.append(loss)
+    return self
+
+def net_inpt(self, X):
+    """Calculate the net input
+    """
+    return np.dot(X, self.w_) + self.b_
+
+def activation(self,X):
+    """Compute linear activation
+    """
+    return X
+
+def predict(self, X):
+    """Return class label after unit step
+    """
+    return np.where(self.activation(self.net_input(X))
+                    >= -.5, 1, 0)
